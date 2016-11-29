2c157f78721a564b7bc9d5d1d0066e1ec8953d15
emiel vandenbussche <emielPC@MacBook-Pro-van-emiel-2.local>
2016-11-29 13:17:48 +0100

    problemen met build.gradle

---
+++ app/src/main/java/com/vandenbussche/emiel/projectsbp/gui/fragments/ProfileFragment.java
@@ -0,0 +1,30 @@
+package com.vandenbussche.emiel.projectsbp.gui.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vandenbussche.emiel.projectsbp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
